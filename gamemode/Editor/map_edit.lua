136ab1c12175c34571f0f409e17a24dd5b22767c
GeekWithALife <ssjgamemaker@charter.net>
2010-08-26 22:14:39 -0400

    Updated README.TXT, fixed numerous problems in the modules system, removed a few examples and added some new ones.

--- gamemode/Editor/map_edit.lua
+++ gamemode/Editor/map_edit.lua
@@ -20,6 +20,127 @@ local function SetupDirs()
 	end
 end
 
+local function EntityDataToString( ent )
+	local function FindType( var )
+		return type(var)
+	end
+	local function DataToString( data )
+		local dtype = FindType( data )
+		if dtype == "Vector" then
+			return "Vector: " .. data.x .. " " .. data.y .. " " .. data.z
+		elseif dtype == "Angle" then
+			return "Angle: " .. data.pitch .. " " .. data.yaw .. " " .. data.roll
+		elseif dtype == "Color" then
+			return "Color: " .. data.r .. " " .. data.g .. " " .. data.b .. " " .. data.a
+		elseif dtype == "keyvalue" then
+			return data[1].." = " .. DataToString( FindType(data[2]), data[2] )
+		elseif dtype == "table" then
+			return glon.encode( data )
+		else
+			return data
+		end
+	end
+	local function AddData( data )
+		return "\t\""..data[1].."\"\t\""..data[2].."\"\n"
+	end
+	local t, s = {}, ""
+	s = "\"id\"\t\""..ent:EntIndex().."\"\n{"
+	s = s.."\t"
+	s = s.."}"
+	
+	local args = {...}
+	
+	if edit == "class" then
+		
+		local kv
+		if ent and ValidEntity( ent ) then
+			kv = ent:GetKeyValues()
+			RemoveMapEntity( ent, true )
+		elseif type( ent ) == "table" then
+			kv = ent.KeyValues
+		end
+		
+		ent = CreateMapEntity( args[1] )
+		
+		for k, v in pairs( kv ) do
+			ent:SetKeyValue( k, v )
+		end
+		
+	elseif edit == "position" then
+		
+		ent:SetPos( Vector( unpack( value ) ) )
+		
+	elseif edit == "angles" then
+		
+		ent:SetAngles( Angle( unpack( value ) ) )
+		
+	elseif edit == "flag" then
+		
+		local flags = 0
+		for i = 1, table.Count( args ) do
+			flags = flags + args[i]
+		end
+		
+		ent:SetKeyValue("spawnflags", flags)
+		
+	elseif edit == "key" then
+	
+		local key = args[1]
+		local valArgs = table.Copy( args )
+		table.remove( valArgs, 1 )
+		local value = valArgs
+		
+		if key == "name" then
+			
+			ent:SetName( value )
+			
+		elseif key == "parent" then
+			
+			ent:SetParent( value )
+			
+		elseif key == "color" then
+			
+			ent:SetColor( Color( unpack( value ) ) )
+			
+		elseif key == "collisiongroup" then
+			
+			ent:SetCollisionGroup( _G[value] )
+			
+		elseif key == "material" then
+			
+			ent:SetMaterial( value )
+			
+		elseif key == "nwvar" then
+			
+			ent["SetNW"..value]( unpack( value ) )
+			
+		elseif key == "dtvar" then
+			
+			ent["SetDT"..value]( unpack( value ) )
+			
+		elseif key == "skin" then
+			
+			ent:SetSkin( value )
+			
+		elseif key == "command" then
+			
+			ent[key]( unpack(value) )
+			
+		else
+			
+			ent:SetKeyValue( key, value )
+			
+		end
+		
+	elseif edit == "io" then
+		
+	elseif edit == "code" then
+		
+		ent[args[1]] = args[2]
+		
+	end
+end
+
 SetupDirs()
 
 NARWHAL.__MapEditor = {}
