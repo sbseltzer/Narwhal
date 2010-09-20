4cbf7dc96f6203d743be4e3ddd4fd90843cdf7e3
GeekWithALife <ssjgamemaker@charter.net>
2010-09-19 20:14:08 -0400

    - Fixed the problem where naming the narwhal folder incorrectly crashed the game and/or caused other internal issues. - Modules and Themes are now successfully loaded before the gamemode has initialized. - Modules and Themes can be reloaded at any time with a global function. - Removed NARWHAL.IncludeModule. Not really useful since NARWHAL.GetModule basically does the same thing. - Fixed Modules and Themes from narwhal bases not loading. - Added some extra Module load precautions. - Networking system has also been tightened up. - New options for network filters (functions and enumerations). - Players who qualify for a networked var's filter now receive them when joining. - The network retry system had a few logical errors that are now fixed. - Implemented player body and interface for animations system. - Updated the readme.txt with more detailed credits and such. - Updated the info.txt a tiny bit.

--- gamemode/player_shd.lua
+++
@@ -1,8 +0,0 @@
-
-/*---------------------------------------------------------
-   Name: gamemode:PlayerConnect( )
-   Desc: Player has connects to the server (hasn't spawned)
----------------------------------------------------------*/
-function GM:PlayerConnect( name, address )
-
-end
